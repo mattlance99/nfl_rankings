c622465d96d5ff3c9c8c18f14025d8241484cb6d
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-27 21:05:22 +0000

    added to Team class

--- lib/nfl_rankings/team.rb
+++ lib/nfl_rankings/team.rb
@@ -1,12 +1,16 @@
 class NflRankings::Team
-  
-  attr_accessor :city, :mascot, :team_url
+
+  attr_accessor :city, :mascot, :team_url, :passing_yards, :rushing_yards, :receiving_yards
   @@all = []
 
   def initialize(city, mascot, team_url)
     @city = city
     @mascot = mascot
     @team_url = team_url
+    @passing_yards = passing_yards
+    @rushing_yards = rushing_yards
+    @receiving_yards = receiving_yards
+
     @@all << self
   end
 
