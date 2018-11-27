1498454b901676f2cadccc9ec3e9e3cf42726de2
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-27 15:08:06 +0000

    enhanced scraper

--- lib/nfl_rankings/scraper.rb
+++ lib/nfl_rankings/scraper.rb
@@ -4,20 +4,23 @@ require "pry"
 
 class NflRankings::Scraper
 
+  def self.scrape_main_page
     html = open("https://www.usatoday.com/sports/nfl/rankings/")
     page = Nokogiri::HTML(html)
     teams = []
     my_string = ""
-    page.css(".team_slug")
     my_string = page.css(".team_slug").text
   
     my_string = page.css(".team_slug").each do |team_doc|
       city = team_doc.css('.first_name').text
       mascot = team_doc.css('.last_name').text
-      Team.new(city, mascot)
+      team_url = "https://www.usatoday.com/sports/nfl/#{mascot}"
+      Team.new(city, mascot, team_url)
     end
-
-    Team.all.each_with_index do |team, index|
-      puts "#{index +1 }. #{team.city} #{team.mascot}"
-    end
+    
+  def self.scrape_team_page(team_url)
+    html = open("#{team_url}")
+    team_page = Nokogiri::HTML(html)
+    leading_stats = team_page.css(".sports-team-statistics-chart-titlevalue").children[0..2].map {|stat|stat.text}
+  end
 end
--- lib/nfl_rankings/team.rb
+++ lib/nfl_rankings/team.rb
@@ -2,10 +2,11 @@ class NflRankings::Team
   
   attr_accessor :city, :mascot
   @@all = []
-  
-  def initialize(city, mascot)
+
+  def initialize(city, mascot, team_url)
     @city = city
     @mascot = mascot
+    @team_url = team_url
     @@all << self
   end
 
